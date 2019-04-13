1b6aeae1f4e1d9c279c6280e48212d164be6aec8
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-13 14:46:13 +0200

    Add form validation

--- scripts.js
+++ scripts.js
@@ -1,8 +1,49 @@
 // Add event listeners
 var loading = document.getElementById("loading");
 var form = document.getElementById("form");
+var subject = document.getElementById("subject");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var message = document.getElementById("message");
+
+var users = [
+    { email: "test@test", password: "testtest" }
+];
+
+var resetButtons = function() {
+    subject.classList.remove("form-error");
+    email.classList.remove("form-error");
+    password.classList.remove("form-error");
+    message.innerText = "";
+    message.classList.remove("error");
+    message.classList.remove("success");
+};
 
 var processing = function() {
+    resetButtons();
+
+    email_value = email.value;
+    password_value = password.value;
+
+    var logged = false;
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        console.log(email_value, password_value);
+        console.log(user.email, user.password);
+        if (user.email === email_value && user.password === password_value) {
+            logged = true;
+            break;
+        }
+    }    
+
+    if (logged) {
+        message.innerText = "Zalogowano pomyślnie";
+    } else {
+        message.innerText = "Nieprawidłowy e-mail lub hasło";
+        email.classList.add("form-error");
+        password.classList.add("form-error");
+    }
+
     loading.classList.add("hidden");
 }
 
